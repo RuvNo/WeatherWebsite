ebf44a65177112edf0c0f199cb21696a9c500eb8
Ruven Noack <noack.ruven@gmail.com>
2022-03-09 14:10:09 +0100

    Deleted unnecessary Files and Added First Button

---
+++ weather-website/src/components/LocationButtons/LocationButtons.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LocationButton = () => {
+
+    return (
+        <div>LocationButton</div>
+    )
+}
+
+export default LocationButton
---
+++ weather-website/src/components/Website.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import LocationButton from './LocationButtons/LocationButtons'
+
+
+const Website = () => {
+  
+    return (
+        <div>
+            <div>Hallo</div>
+            <LocationButton />
+        </div>
+        
+    )
+}
+
+export default Website
