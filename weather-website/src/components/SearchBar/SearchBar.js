a80834e67bbde40d9645cac71ed46a6cfb5965d8
Ruven Noack <noack.ruven@gmail.com>
2022-03-10 12:20:09 +0100

    Added functionality for the City Search
    Using a List of all cities with a population of 1000+, you can now see recommendations for your city search

--- weather-website/src/components/SearchBar/SearchBar.js
+++ weather-website/src/components/SearchBar/SearchBar.js
@@ -1,23 +1,15 @@
 import React, {useEffect, useState} from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
 
     const [searchValue, setSearchValue] = useState("")
-
-    const cityList = [
-        'Wuppertal',
-        'Berlin',
-        'Münster',
-        'Hamburg',
-        'Worms',
-        'Hannover'
-    ]
 
     const handleInputChange = (event) => {
         setSearchValue(event.target.value)
     }
 
-    const filteredCityList = cityList.filter((city) => city.includes(searchValue))
+    let filteredCityList = props.cityList.filter((city) => city[0].includes(searchValue))
+    filteredCityList = filteredCityList.sort(() => Math.random() - 0.5).slice(0,10)
 
     const shouldDisplayButton = searchValue.length > 0
 
@@ -31,19 +23,15 @@ const SearchBar = () => {
         document.getElementById("Long").textContent = arr[1]
     })
 
-    // ATM Muss ich 2x Klicken, damit es funktioniert -> Also "City" klicken, dann irgendwas eingeben (z.B. Koordinaten)
-    // Dann "Search", dann kommt nichts, weil newLat und newLon noch keine Daten haben und dann muss ich nochmal "Search" 
-    // drücken, damit es funktioniert.
-    // --> D.h. irgendwo muss ich meine Informationen früher weitergeben/aktualisieren! Mal sehen, wie und wo
-
     // Abgesehen davon muss ich dafür sorgen, dass er nur versucht zu splitten etc (mit dem ","), wenn Koordinaten ausgesucht ist
 
 
     return (
         <div>
             <input type="text" id="SearchValue" value={searchValue} onChange={handleInputChange} />
+            <div id = "SearchValue2">{searchValue}</div>
             {shouldDisplayButton && <button onClick={handleClearClick}>Clear</button>}
-            {filteredCityList.map((city) => {
+            {shouldDisplayButton && filteredCityList.map((city) => {
                 return <li key={city}>{city}</li>
             })}           
         </div>
--- weather-website/src/components/Website.js
+++ weather-website/src/components/Website.js
@@ -2,10 +2,36 @@ import React, { useEffect, useState } from 'react'
 import SearchBar from './SearchBar/SearchBar'
 import Displays from './Display/Display'
 import OptionButton from './OptionButton/OptionButton'
+import cityAndCountryNames from './CityNames/CityNames'
 
+// 1. Aktuell funktioniert es nicht, erst eine Sache zu suchen und dann eine andere (also erst Wuppertal und dann Köln z.B.)
+// 2. Es muss noch eingestellt werden, dass man auch seine Location nutzen kann
+// 3. Das Interface muss aufgeräumt werden
+// 4. Import von Icons
+// 5. Hübsch machen
+// 6. zusätzliche Feature?! 
 
 const Website = () => {
-  
+
+    function geoLocation() {
+        var x = document.getElementById("demo");
+        var y = document.getElementById("demo2");
+    
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            } else {
+                x.innerHTML = "Geolocation is not supported by this browser.";
+            }
+        }
+
+        function showPosition(position) {
+            x.innerHTML = position.coords.latitude;
+            y.innerHTML = position.coords.longitude;
+        }
+        getLocation()
+    }
+      
     const [cityObj, setCityObj] = useState([])
     const [cityObj2, setCityObj2] = useState([])
     const [searchValue, setSearchValue] = useState("empty")
@@ -16,7 +42,7 @@ const Website = () => {
        
     useEffect(() => {
         if(long !== 0 || lat !== 0) {
-            fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=5576b5d04897ed986fb56430d37779ec')
+            fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&units=metric&appid=5576b5d04897ed986fb56430d37779ec')
                 .then(res=>res.json())
                 .then((cityJSON) => {
                     const cityInformation = [cityJSON.name, cityJSON.main.temp]
@@ -34,7 +60,7 @@ const Website = () => {
         // Hier sollte man nochmal schauen, das mit dem newLat und newLon ist alles super doppelt gemoppelt
         // Also hier gibt es einen effizienteren Weg, das ganze darzustellen
         if(newLong !== 0 || newLat !== 0) {
-            fetch('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=5576b5d04897ed986fb56430d37779ec')
+            fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + document.getElementById("SearchValue2").textContent + '&limit=5&appid=5576b5d04897ed986fb56430d37779ec')
                 .then(res=>res.json())
                 .then((cityJSON) =>    {        
                     const cityInformation2 = [cityJSON[0].lat, cityJSON[0].lon]
@@ -45,7 +71,7 @@ const Website = () => {
                     setCityObj2(cityInformation2)
                 });
             if(newLong !== "" && newLat !=="") {
-            fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + newLat + '&lon=' + newLong + '&appid=5576b5d04897ed986fb56430d37779ec')
+            fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + newLat + '&lon=' + newLong + '&units=metric&appid=5576b5d04897ed986fb56430d37779ec')
                 .then(res=>res.json())
                 .then((cityJSON) => {
                     const cityInformation = [cityJSON.name, cityJSON.main.temp]
@@ -57,6 +83,7 @@ const Website = () => {
     }, [newLong])
 
     function searchCity() {
+        console.log(document.getElementById("SearchValue2").textContent)
         if (document.getElementById("searchType").textContent === "Search Type: Coordinates") {
             setLat(document.getElementById("Lat").innerHTML)
             setLong(document.getElementById("Long").innerHTML)
@@ -64,7 +91,8 @@ const Website = () => {
             setNewLat(document.getElementById("newLat").innerHTML)
             setNewLong(document.getElementById("newLon").innerHTML)
         } else {
-            console.log("WHY")
+            setLat(document.getElementById("demo").innerHTML)
+            setLong(document.getElementById("demo2").innerHTML)
         }
         
     }
@@ -72,21 +100,24 @@ const Website = () => {
     let val = ""
     return (
         <div>
+            <div id="demo">Test</div>
+            <div id="demo2">Test</div>
             <div id="Lat"></div>
             <div id="Long"></div>
             <div id="newLat"></div>
             <div id="newLon"></div>
             <div id="searchType">Search Type: </div>
-            <SearchBar />
+            <SearchBar cityList={cityAndCountryNames}/>
             <OptionButton name={"Coordinates"}/>
             <OptionButton name={"City"}/>
             <OptionButton name={"myLocation"}/>
             <Displays feature={cityObj[0]}/>
             <Displays feature={cityObj[1]}/>
-            <Displays feature={cityObj2[0]}/>
-            <Displays feature={cityObj2[1]}/>
+            {/* <Displays feature={cityObj2[0]}/>
+            <Displays feature={cityObj2[1]}/> */}
             {/* <Displays feature={cityObj[2]}/> */}
             <button onClick={searchCity}>Search</button>
+            <button onClick={geoLocation}>GetLocation</button>
             <div>{searchValue}</div>
         </div>
         
